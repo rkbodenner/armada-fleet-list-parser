899f4fd65938960db198bf5487968796c306f216
Ralph Bodenner <rkbodenner+github@gmail.com>
2020-02-12 08:30:32 -0800

    Parse Armada Fleet Builder iOS app export format

---
+++ armada-fleet-builder-fleet.pegjs
@@ -0,0 +1,115 @@
+start
+  = name:ListName pts:Points faction:Faction author:Author commander:Commander objectives:Objectives? ships:Ship* sqd:Squadrons {
+    var points = pts.points;
+    var max_points = pts.max_points;
+
+    var squadrons = sqd.squads;
+    var squadron_points = sqd.points;
+
+    if ( ! objectives ) { objectives =  []; }
+
+    return {name, author, faction, points, max_points, squadron_points, commander, objectives, ships, squadrons};
+  }
+
+ListName
+  = name:Name [ \n\t]* { return name; }
+
+Author
+  = "Author:" _ name:Name _ [\n]* { return name; }
+
+Faction
+  = value:FactionValue _ "-" _ { return value; }
+
+FactionValue
+  = RebelFaction / ImperialFaction
+
+ImperialFaction
+  = "Empire" { return "Galactic Empire"; }
+
+RebelFaction
+  = "Rebels" { return "Rebel Alliance"; }
+
+Points
+  = _ "(" points:Integer "/" max_points:Integer ")" [ \n\t]* { return {points, max_points}; }
+
+Commander
+  = "Commander:" _ name:CardName [\n]* { return name; }
+
+Objectives
+  = "Objectives:" _ objs:(AssaultObjective DefenseObjective NavigationObjective) [\n]* { return objs; }
+
+AssaultObjective
+  = _ name:Name "," _ [\n]* {
+    var type = "Assault";
+    return {type, name};
+  }
+
+DefenseObjective
+  = _ name:Name "," _ [\n]* {
+    var type = "Defense";
+    return {type, name};
+  }
+
+NavigationObjective
+  = _ name:Name _ [\n]* {
+    var type = "Navigation";
+    return {type, name};
+  }
+
+
+Objective
+  = type:ObjectiveType _ "Objective:" _ name:Name _ [\n]* { return {type: type, name: name}; }
+
+ObjectiveType
+  = "Assault"/"Defense"/"Navigation"
+
+Ship
+  = flagship:Flagship? ship_class:ShipClass upgrades:Upgrade* points:TotalCost [\n]* {
+    if (flagship) { return { flagship, ship_class, upgrades, points }; }
+    else          { return {           ship_class, upgrades, points }; }
+  }
+
+Flagship
+  = "[" _ "flagship" _ "]" _ { return true; }
+
+ShipClass
+  = name:Name _ points:Cost _ [\n]? { return {name: name, points: points}; }
+
+Upgrade
+  = "-" _ name:CardName _ points:Cost _ [\n]? { return {name: name, points: points}; }
+
+Cost
+  = "(" _ cost:Integer _ ")" _ [\n]? { return cost; }
+
+TotalCost
+  = "=" _ totalCost:Integer _ "total points" _ [\n]? { return totalCost; }
+
+Squadrons
+  = "Squadrons (" points:Integer "/" Integer "):" [\n]* squads:Squadron* _ [\n]? { return {squads, points}; }
+
+Squadron
+  = count:Integer "x" _ name:CardName _ cost:Cost _ [\n]? { return {count, name, cost}; }
+
+NameVariant
+  = "com"/"off"
+
+CardName
+  = name:Name _ variant:(("(" NameVariant ")")?) {
+  	if (variant) {
+      return name.concat(" ", variant.join(""));
+    } else {
+      return name;
+    }
+  }
+
+Name
+  = words:(NameWord)* [ \n\t]* { return words.join(" "); }
+
+NameWord
+  = chars:[-_a-zA-Z0-9!']+ _ { return chars.join(""); }
+
+Integer "integer"
+  = [0-9]+ { return parseInt(text(), 10); }
+
+_ "whitespace"
+  = [ \t]*
--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ const assert = require("assert");
 
 const warlordsParser = require("../armada-warlords-fleet.js");
 const kingstonParser = require("../ryan-kingston-fleet.js");
+const afbParser = require("../armada-fleet-builder-fleet.js");
 
 describe("Armada Warlords", function() {
   var parser = warlordsParser;
@@ -49,19 +50,36 @@ describe("Ryan Kingston's", function() {
   });
 });
 
+describe("Armada Fleet Builder", function() {
+  var parser = afbParser;
+  describe("Well-formed lists", function() {
+    it("Should parse raddeux-lib-v4", function() {
+      var list = fs.readFileSync("test/armada-fleet-builder/raddeux-lib-v4.txt");
+      assert.doesNotThrow(() => {
+        parser.parse(list.toString("utf8"));
+      });
+    });
+  });
+});
+
 describe("Data format consistency", function() {
   describe("Dumpster fire", function() {
-    it("Should produce consistent output with Kingston and Warlords versions", function() {
+    it("Should produce consistent output with Armada Fleet Builder, Kingston, and Warlords versions", function() {
+      var afbList = fs.readFileSync("test/armada-fleet-builder/dumpster-fire-mk-18.txt");
+      var afbFleet = afbParser.parse(afbList.toString("utf8"));
+
       var kingstonList = fs.readFileSync("test/ryan-kingston/dumpster-fire-mk-18.txt");
       var kingstonFleet = kingstonParser.parse(kingstonList.toString("utf8"));
 
       var warlordsList = fs.readFileSync("test/warlords/dumpster-fire-mk-18.txt");
       var warlordsFleet = warlordsParser.parse(warlordsList.toString("utf8"));
 
-      // Warlords removes apostrophes from names! Normalize Kingston to match.
-      kingstonFleet.ships.forEach(function(ship) {
-        ship.upgrades.forEach(function(upgrade) {
-          upgrade.name = upgrade.name.replace("'","");
+      // Warlords removes apostrophes from names! Normalize Kingston and AFB to match.
+      [afbFleet,kingstonFleet].forEach(function(fleet) {
+        fleet.ships.forEach(function(ship) {
+          ship.upgrades.forEach(function(upgrade) {
+            upgrade.name = upgrade.name.replace("'","");
+          });
         });
       });
 
@@ -70,9 +88,22 @@ describe("Data format consistency", function() {
         squad.name = squad.name.replace("Squadrons", "Squadron");
       });
 
-      // Warlords capitalizes "wing" in "X-wing". Normalize to the name as printed on the card.
-      warlordsFleet.squadrons.forEach(function(squad) {
-        squad.name = squad.name.replace("-Wing", "-wing");
+      // Warlords and AFB capitalize "wing" in "X-wing". Normalize to the name as printed on the card.
+      [afbFleet,warlordsFleet].forEach(function(fleet) {
+        fleet.squadrons.forEach(function(squad) {
+          squad.name = squad.name.replace("-Wing", "-wing");
+        });
+      });
+
+      // AFB adds the model name for some ship classes. Use just the class.
+      afbFleet.ships.forEach(function(ship) {
+        ship.ship_class.name = ship.ship_class.name.replace("MC80 Liberty","MC80");
+      });
+
+      // AFB adds the model name for some squadron uniques. Use just the class.
+      afbFleet.squadrons.forEach(function(squad) {
+        squad.name = squad.name.replace(" X-wing Squadron","");
+        squad.name = squad.name.replace("Moldy Crow - ","");
       });
 
       // Kingston uses shorthand for some ship classes. Use the full names.
@@ -83,18 +114,28 @@ describe("Data format consistency", function() {
       });
 
       // Remove properties that Kingston doesn't include
-      warlordsFleet.ships.forEach(function(ship) {
-        delete ship.flagship;
+      [afbFleet,warlordsFleet].forEach(function(fleet) {
+        fleet.ships.forEach(function(ship) {
+          delete ship.flagship;
+        });
       });
 
       // Order of upgrades is not guaranteed to match
-      [kingstonFleet,warlordsFleet].forEach(function(fleet) {
+      [afbFleet,kingstonFleet,warlordsFleet].forEach(function(fleet) {
         fleet.ships.forEach(function(ship) {
           ship.upgrades.sort(function(a,b) {
             return ('' + a.name).localeCompare(b.name);
           });
         });
       });
+
+      // Order of ships is not guaranteed to match
+      [afbFleet,kingstonFleet,warlordsFleet].forEach(function(fleet) {
+        fleet.ships.sort(function(a,b) {
+          return ('' + a.ship_class.name).localeCompare(b.ship_class.name);
+        });
+      });
+
 
       var matchingProps = [
         'name',
@@ -106,6 +147,8 @@ describe("Data format consistency", function() {
         'squadrons'
       ];
       matchingProps.forEach(function(property){
+        assert.deepStrictEqual(afbFleet[property], warlordsFleet[property]);
+        assert.deepStrictEqual(afbFleet[property], kingstonFleet[property]);
         assert.deepStrictEqual(kingstonFleet[property], warlordsFleet[property]);
       });
     });
